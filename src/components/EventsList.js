0fd60243d2ef9fa1c4170d103b33d23759ea33cc
PkuCuipy <pkucuipy@icloud.com>
2024-11-30 18:35:53 -0800

    optimized event card UI: 1. hover to see full description 2. assign color to emoji by hash

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -11,6 +11,14 @@ const formatSeconds = (seconds) => {
   return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}:${s.toString().padStart(2, '0')}`;
 }
 
+const textToColor = (text) => {
+  /* Generate a color from the hash of the text */
+  const colors = ["bg-red-200", "bg-orange-200", "bg-yellow-200", "bg-lime-200", "bg-green-200", "bg-teal-200", "bg-blue-200", "bg-indigo-200", "bg-purple-200", "bg-pink-200"];
+  const hash = text.split("").reduce((acc, c) => acc + c.charCodeAt(0), 0);
+  return colors[hash % colors.length];
+}
+
+
 const EventBlock = ({ event, seekTo }) => {
   //   *------------------------------------*
   //   |          | Title       |           |
@@ -20,38 +28,49 @@ const EventBlock = ({ event, seekTo }) => {
   const [hovered, setHovered] = useState(false);
 
   return (
-    <div className="flex w-full my-1.5 items-center justify-between rounded-xl bg-gray-900 select-none cursor-pointer hover:opacity-90"
-         onClick={() => seekTo(event.startTime)}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
+    <div
+      className="flex w-full my-1.5 items-center justify-between rounded-xl bg-gray-900 select-none cursor-pointer hover:opacity-90"
+      onClick={() => seekTo(event.startTime)}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
     >
-      {/* Left Icon*/}
-      <div className="w-[50px] h-[50px] mx-4 my-4 bg-gray-500 rounded-full flex items-center justify-center">
-        {event.type}
-      </div>
-
-      {/* Middle Text*/}
-      <div className="mr-1">
-        <p className="text-md font-bold max-w-[160px] overflow-hidden whitespace-nowrap overflow-ellipsis">{event.title}</p>
-        <p className="text-sm max-w-[160px] overflow-hidden whitespace-nowrap overflow-ellipsis">{event.description}</p>
-        <p className="text-sm">{formatSeconds(event.startTime)}</p>
-      </div>
-
-      {/* Space */}
-      <div className="flex-1"/>
-
-      {/* Right Thumbnail*/}
-      <div className="w-[100px] h-[70px] m-2 bg-gray-500 flex items-center justify-center rounded-lg relative cursor-pointer overflow-hidden">
-        <img className="absolute w-full h-full object-fit"
-             src={`https://picsum.photos/200/300`}
-              alt="Thumbnail"
-        />
-        {hovered && (
-          <div className="absolute bg-neutral-900 text-neutral-100 bg-opacity-80 text-2xl py-0.5 px-3 rounded-lg">
-            ▶
+      {(hovered === false)
+        ?
+        <>
+          {/* Left Icon*/}
+          <div className={`${textToColor(event.type)} w-[50px] h-[50px] mx-4 my-4 bg-gray-500 rounded-full flex items-center justify-center text-2xl`}>
+            {event.type}
           </div>
-        )}
-      </div>
+          {/* Middle Text*/}
+          <div className="mr-1">
+            <p className="text-md font-bold max-w-[160px] overflow-hidden whitespace-nowrap overflow-ellipsis">{event.title}</p>
+            {/*<p className="text-sm max-w-[160px] overflow-hidden whitespace-nowrap overflow-ellipsis">{event.description}</p>*/}
+            <p className="text-sm">{formatSeconds(event.startTime)}</p>
+          </div>
+          {/* Space */}
+          <div className="flex-1"/>
+          {/* Right Thumbnail*/}
+          <div className="w-[100px] h-[70px] m-2 bg-gray-500 flex items-center justify-center rounded-lg relative cursor-pointer overflow-hidden">
+            <img className="absolute w-full h-full object-fit" src={`https://picsum.photos/200/300`} alt="Thumbnail"/>
+          </div>
+        </>
+        :
+        <>
+          {/* Space */}
+          <div className="flex-1"/>
+          {/* Text*/}
+          <div className="ml-4">
+            <p className="text-sm max-w-[230px] max-h-[60px] overflow-auto">{event.description}</p>
+          </div>
+          {/* Space */}
+          <div className="flex-1"/>
+          {/* Right Thumbnail*/}
+          <div className="w-[100px] h-[70px] m-2 bg-gray-500 flex items-center justify-center rounded-lg relative cursor-pointer overflow-hidden">
+            <img className="absolute w-full h-full object-fit opacity-70 transition-opacity" src={`https://picsum.photos/200/300`} alt="Thumbnail"/>
+            <div className="absolute bg-neutral-900 text-neutral-100 bg-opacity-70 text-2xl py-0.5 px-3 rounded-lg"> ▶ </div>
+          </div>
+        </>
+      }
     </div>
   );
 }
@@ -60,7 +79,7 @@ const EventBlock = ({ event, seekTo }) => {
 const fakeEvents = [
   {
     title: 'Cat Appears',
-    description: 'Description',
+    description: 'Description very very very very very very very very very very very very very very very very very very long',
     startTime: 123.12,
     type: '🐱',
     objects: ['Cat', 'Table', 'Chair', 'Lamp'],
